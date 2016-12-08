4b595f2a6bff7401432ff27119a5c5451f77f35a
Calvin Leung Huang <cleung2010@gmail.com>
2016-12-07 23:54:31 -0500

    Day 7

--- day7/main.go
+++ day7/main.go
@@ -8,26 +8,6 @@ import (
 	"regexp"
 )
 
-// From: https://gobyexample.com/collection-functions
-func Filter(vs []string, f func(string) bool) []string {
-	vsf := make([]string, 0)
-	for _, v := range vs {
-		if f(v) {
-			vsf = append(vsf, v)
-		}
-	}
-	return vsf
-}
-
-func All(vs []string, f func(string) bool) bool {
-	for _, v := range vs {
-		if !f(v) {
-			return false
-		}
-	}
-	return true
-}
-
 func IsABBA(input string) bool {
 	for i := 0; i < len(input)-3; i++ {
 		if input[i] == input[i+3] && input[i+1] == input[i+2] && input[i] != input[i+1] {
@@ -59,7 +39,6 @@ func IsTLS(input string) bool {
 	potentialStrings := re.Split(currentLine, -1)
 	fmt.Printf("%s\n", currentLine)
 
-	// nextScan := false
 	// If it's bracket string is ABBA go to next scan
 	for _, submatch := range bracketStrings {
 		if IsABBA(submatch[1]) {
