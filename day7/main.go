2c924628621ff210e3dbb46106f776b18120421e
Calvin Leung Huang <cleung2010@gmail.com>
2016-12-07 23:53:29 -0500

    Day 7

--- day7/main.go
+++ day7/main.go
@@ -38,46 +38,106 @@ func IsABBA(input string) bool {
 	return false
 }
 
+func IsXYX(input string) []string {
+	abaCollection := []string{}
+	for i := 0; i < len(input)-2; i++ {
+		if input[i] == input[i+2] && input[i] != input[i+1] {
+			byteSlice := []byte{input[i], input[i+1], input[i]}
+			abaCollection = append(abaCollection, string(byteSlice))
+		}
+	}
+
+	return abaCollection
+}
+
+func IsTLS(input string) bool {
+	re := regexp.MustCompile("\\[([^\\]]*)\\]")
+
+	// Parse each character
+	currentLine := input
+	bracketStrings := re.FindAllStringSubmatch(currentLine, -1)
+	potentialStrings := re.Split(currentLine, -1)
+	fmt.Printf("%s\n", currentLine)
+
+	// nextScan := false
+	// If it's bracket string is ABBA go to next scan
+	for _, submatch := range bracketStrings {
+		if IsABBA(submatch[1]) {
+			// log.Println("Invalid hypernet")
+			return false
+		}
+	}
+
+	for _, submatch := range potentialStrings {
+		if IsABBA(submatch) {
+			// log.Println("Is ABBA")
+			return true
+		}
+	}
+
+	return false
+}
+
+func IsSSL(input string) bool {
+	re := regexp.MustCompile("\\[([^\\]]*)\\]")
+
+	currentLine := input
+	bracketStrings := re.FindAllStringSubmatch(currentLine, -1)
+	potentialStrings := re.Split(currentLine, -1)
+
+	log.Println(currentLine)
+
+	// Get abas
+	abas := []string{}
+	for _, submatch := range potentialStrings {
+		res := IsXYX(submatch)
+		abas = append(abas, res...)
+	}
+	log.Printf("abas: %q\n", abas)
+
+	// Get babs
+	babs := []string{}
+	for _, submatch := range bracketStrings {
+		res := IsXYX(submatch[1])
+		babs = append(babs, res...)
+	}
+	log.Printf("babs: %q\n", babs)
+
+	// Do comparison
+	for _, aba := range abas {
+		for _, bab := range babs {
+			log.Printf("aba: %s | bab: %s", aba, bab)
+			if aba[0] == bab[1] && aba[1] == bab[0] {
+				return true
+			}
+		}
+	}
+
+	return false
+}
+
 func main() {
-	count := 0
+	countPart1, countPart2 := 0, 0
 	file, err := os.Open("data")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer file.Close()
 
-	re := regexp.MustCompile("\\[([^\\]]*)\\]")
-
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		// Parse each character
-		// fmt.Println(scanner.Text())
-		currentLine := scanner.Text()
-		bracketStrings := re.FindAllStringSubmatch(currentLine, -1)
-		potentialStrings := re.Split(currentLine, -1)
-		fmt.Printf("%s\n", currentLine)
-
-		nextScan := false
-		// If it's bracket string is ABBA go to next scan
-		for _, submatch := range bracketStrings {
-			if IsABBA(submatch[1]) {
-				log.Println("Invalid hypernet")
-				nextScan = true
-				break
-			}
+		input := scanner.Text()
+		// Part 1
+		if IsTLS(input) {
+			countPart1++
 		}
-		if nextScan {
-			continue
+		// Part 2
+		if IsSSL(input) {
+			countPart2++
 		}
 
-		for _, submatch := range potentialStrings {
-			if IsABBA(submatch) {
-				fmt.Println("Is ABBA")
-				count++
-				break
-			}
-		}
 	}
 
-	fmt.Println(count)
+	fmt.Printf("Part 1: %d\n", countPart1)
+	fmt.Printf("Part 2: %d\n", countPart2)
 }
